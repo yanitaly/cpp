702d8db7fcf8fd58a45f853c3d4132696d9ec729
yanitaly <44038346+yanitaly@users.noreply.github.com>
2023-04-11 18:18:43 +0200

    Create helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+// C++ program to display "Hello World"
+// compile with: g++ -o helloworld.o helloworld.cpp 
+// run with: helloworld.o
+
+//  // Header file for input output functions
+#include <iostream>
+using namespace std;
+
+// Main() function: where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout << "Hello World" << endl;
+
+    return 0;
+}
