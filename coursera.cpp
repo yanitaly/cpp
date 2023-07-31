383ae3a1b268a5d174f3536d2c4063eb0e87e867
yanitaly <44038346+yanitaly@users.noreply.github.com>
2023-07-31 18:49:10 +0200

    Update coursera.cpp

--- coursera.cpp
+++ coursera.cpp
@@ -7,3 +7,32 @@ int main(){
 
 }
 
+=============
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv) {
+  
+  vector<string> reverse(0);
+  reverse.push_back(argv[1]);
+  reverse.push_back(argv[2]);
+  reverse.push_back(argv[3]);
+  
+  //add code below this line
+  for(int i=0; i<3; i++){
+    reverse.push_back(reverse[2-i]);
+  }
+
+  for(int i=0; i<3; i++){
+    reverse.erase(reverse.begin());
+  }
+  //add code above this line
+  
+  for (auto a : reverse) {
+    cout << a << endl;
+  }
+  
+  return 0;
+  
+}
